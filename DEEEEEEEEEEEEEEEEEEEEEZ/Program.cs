a9f8d72a9932142dd3bf51c7e52eff1565bdbb14
Xtroolik <xtroolik@gmail.com>
2023-03-20 12:36:58 +0100

    DnD Dice

--- DEEEEEEEEEEEEEEEEEEEEEZ/Program.cs
+++ DEEEEEEEEEEEEEEEEEEEEEZ/Program.cs
@@ -2,6 +2,10 @@
 Console.WriteLine("1. D3");
 Console.WriteLine("2. D4");
 Console.WriteLine("3. D6");
+Console.WriteLine("4. D10");
+Console.WriteLine("5. D12");
+Console.WriteLine("6. D20");
+Console.WriteLine("7. D100");
 string choice = Console.ReadLine();
 Random rng = new Random();
 int randomThrow = 0;
@@ -19,6 +23,22 @@ switch(choice)
     case "3":
         randomThrow = rng.Next(1, 6);
         Console.WriteLine("Rzut kością D6: " + randomThrow.ToString());
+        break;
+    case "4":
+        randomThrow = rng.Next(1, 10);
+        Console.WriteLine("Rzut kością D10: " + randomThrow.ToString());
+        break;
+    case "5":
+        randomThrow = rng.Next(1, 12);
+        Console.WriteLine("Rzut kością D12: " + randomThrow.ToString());
+        break;
+    case "6":
+        randomThrow = rng.Next(1, 20);
+        Console.WriteLine("Rzut kością D20: " + randomThrow.ToString());
+        break;
+    case "7":
+        randomThrow = rng.Next(1, 100);
+        Console.WriteLine("Rzut kością D100: " + randomThrow.ToString());
         break;
     default:
         Console.WriteLine("Podałeś złą opcję!");
